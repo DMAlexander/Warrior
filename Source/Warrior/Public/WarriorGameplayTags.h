5d51d35797b06e797a6238245603ebb062313ae7
Devin Alexander <devinmalexander@gmail.com>
2025-08-09 08:58:57 -0500

    Completed "Notify Melee Hit"

--- Source/Warrior/Public/WarriorGameplayTags.h
+++ Source/Warrior/Public/WarriorGameplayTags.h
@@ -27,7 +27,9 @@ namespace WarriorGameplayTags
 
 	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Status_JumpToFinisher);
 
-	/** Enemy Tags **/
-
+	/** Enemy tags **/
 	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Enemy_Weapon);
+
+	/** Shared tags **/
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Shared_Event_MeleeHit);
 }
