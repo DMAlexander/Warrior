bcb8ab51e89418d38d225d092f0639dd3d9eabbe
Devin Alexander <devinmalexander@gmail.com>
2025-08-09 11:42:49 -0500

    Completed "Hero Damage Info"

--- Source/Warrior/Private/Components/Combat/HeroCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/HeroCombatComponent.cpp
@@ -14,6 +14,15 @@ AWarriorHeroWeapon* UHeroCombatComponent::GetHeroCarriedWeaponByTag(FGameplayTag
     return Cast<AWarriorHeroWeapon>(GetCharacterCarriedWeaponByTag(InWeaponTag));
 }
 
+AWarriorHeroWeapon *UHeroCombatComponent::GetHeroCurrentEquippedWeapon() const
+{
+    return Cast<AWarriorHeroWeapon>(GetCharacterCurrentEquippedWeapon());
+}
+
+float UHeroCombatComponent::GetHeroCurrentEquippWeaponDamageAtLevel(float InLevel) const
+{
+    return GetHeroCurrentEquippedWeapon()->HeroWeaponData.WeaponBaseDamage.GetValueAtLevel(InLevel);
+}
 
 void UHeroCombatComponent::OnHitTargetActor(AActor *HitActor)
 {
