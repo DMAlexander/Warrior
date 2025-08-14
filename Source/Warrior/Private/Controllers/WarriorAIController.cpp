cf646180615a4bd17c955c78296169def5dbd1e9
Devin Alexander <devinmalexander@gmail.com>
2025-08-14 16:26:21 -0500

    Completed "Custom Query Context"

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -34,7 +34,7 @@ ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& O
 
     const IGenericTeamAgentInterface* OtherTeamAgent = Cast<IGenericTeamAgentInterface>(PawnToCheck->GetController());
 
-    if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() != GetGenericTeamId())
+    if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() < GetGenericTeamId())
     {
         return ETeamAttitude::Hostile;
     }
@@ -68,12 +68,15 @@ void AWarriorAIController::BeginPlay()
 
 void AWarriorAIController::OnEnemyPerceptionUpdated(AActor *Actor, FAIStimulus Stimulus)
 {
-    if (Stimulus.WasSuccessfullySensed() && Actor)
+    if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
     {
         Debug::Print(Actor->GetActorNameOrLabel() + TEXT(" was sensed"),FColor::Green);
-        if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+        if (!BlackboardComponent->GetValueAsObject(FName("TargetActor")))
         {
-            BlackboardComponent->SetValueAsObject(FName("TargetActor"),Actor);
+            if(Stimulus.WasSuccessfullySensed() && Actor)
+            {
+                BlackboardComponent->SetValueAsObject(FName("TargetActor"),Actor);
+            }
         }
     }
 }
