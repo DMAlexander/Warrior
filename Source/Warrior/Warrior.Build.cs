ccfe19252e98b12e5fb70a10cd63196e72a2dac5
Devin Alexander <devinmalexander@gmail.com>
2025-08-14 16:39:12 -0500

    Completed "Calculate Direction"

--- Source/Warrior/Warrior.Build.cs
+++ Source/Warrior/Warrior.Build.cs
@@ -8,7 +8,7 @@ public class Warrior : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks", "AnimGraphRuntime" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
