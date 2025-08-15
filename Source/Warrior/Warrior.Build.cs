2631a6a7460d5a4e7b63d616e964894f37889962
Devin Alexander <devinmalexander@gmail.com>
2025-08-15 10:16:28 -0500

    Completed "Motion Warping"

--- Source/Warrior/Warrior.Build.cs
+++ Source/Warrior/Warrior.Build.cs
@@ -8,7 +8,7 @@ public class Warrior : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks", "AnimGraphRuntime" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks", "AnimGraphRuntime", "MotionWarping" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
