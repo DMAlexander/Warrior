bbd7f6fca703eca63c0869f9f2370ac4e991b7d7
Devin Alexander <devinmalexander@gmail.com>
2025-08-16 17:29:19 -0500

    Completed "Get Available Targets"

--- Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -3,14 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
 #include "HeroGameplayAbility_TargetLock.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class WARRIOR_API UHeroGameplayAbility_TargetLock : public UWarriorGameplayAbility
+class WARRIOR_API UHeroGameplayAbility_TargetLock : public UWarriorHeroGameplayAbility
 {
 	GENERATED_BODY()
 	
@@ -18,4 +18,23 @@ protected:
 	//~ Begin UGameplayAbility Interface
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+private:
+	void TryLockOnTarget();
+	void GetAvailableActorsToLock();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	float BoxTraceDistance = 5000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	FVector TraceBoxSize = FVector(5000.f,5000.f,300.f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	TArray< TEnumAsByte < EObjectTypeQuery > > BoxTraceChannel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Target Lock")
+	bool bShowPersistentDebugShape = false;
+
+	UPROPERTY()
+	TArray<AActor*> AvailableActorsToLock;
 };
