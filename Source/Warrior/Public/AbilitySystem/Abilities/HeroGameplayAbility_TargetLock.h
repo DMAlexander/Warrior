ddd8d6a8c52a215a99f1ffec5368aac3df159f7c
Devin Alexander <devinmalexander@gmail.com>
2025-08-16 19:13:39 -0500

    Completed "On Target Lock Tick Done"

--- Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/HeroGameplayAbility_TargetLock.h
@@ -21,6 +21,9 @@ protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
+	UFUNCTION(BlueprintCallable)
+	void OnTargetLockTick(float DeltaTime);
+
 private:
 	void TryLockOnTarget();
 	void GetAvailableActorsToLock();
