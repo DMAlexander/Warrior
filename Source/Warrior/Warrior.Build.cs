052b77adf976e2d9ba1d30a17904ca0d9519db54
Devin Alexander <devinmalexander@gmail.com>
2025-08-17 10:13:17 -0500

    Completed "Projectile Class"

--- Source/Warrior/Warrior.Build.cs
+++ Source/Warrior/Warrior.Build.cs
@@ -8,7 +8,7 @@ public class Warrior : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks", "AnimGraphRuntime", "MotionWarping" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks", "AnimGraphRuntime", "MotionWarping", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
