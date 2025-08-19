9c7ca995717aa4ceec88895925ccb3762b8b64ae
Devin Alexander <devinmalexander@gmail.com>
2025-08-19 08:01:29 -0500

    Completed "Toggle Box Collision"

--- Source/Warrior/Public/Components/Combat/PawnCombatComponent.h
+++ Source/Warrior/Public/Components/Combat/PawnCombatComponent.h
@@ -44,6 +44,10 @@ public:
 	virtual void OnWeaponPulledFromTargetActor(AActor* InteractedActor);
 
 protected:
+	virtual void ToggleCurrentEquippedWeaponCollision(bool bShouldEnable);
+	virtual void ToggleBodyCollisionBoxCollision(bool bShouldEnable,EToggleDamageType ToggleDamageType);
+
+protected:
 	TArray<AActor*> OverlappedActors;
 
 private:
