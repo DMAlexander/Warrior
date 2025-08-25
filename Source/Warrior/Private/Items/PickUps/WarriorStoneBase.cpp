368e79dbade061f337c250baa43e0f7b923efcb8
Devin Alexander <devinmalexander@gmail.com>
2025-08-25 18:44:59 -0500

    Completed "Pick Up Ability"

--- Source/Warrior/Private/Items/PickUps/WarriorStoneBase.cpp
+++ Source/Warrior/Private/Items/PickUps/WarriorStoneBase.cpp
@@ -1,5 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Items/WarriorStoneBase.h"
+#include "Items/PickUps/WarriorStoneBase.h"
 
+void AWarriorStoneBase::OnPickUpCollisionSphereBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+}
