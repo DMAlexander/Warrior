2d476d76e76f57b299f9c5b557f42b200e11af67
Devin Alexander <devinmalexander@gmail.com>
2025-08-26 12:38:52 -0500

    Completed "Loading Screen"

--- Source/Warrior/Warrior.Build.cs
+++ Source/Warrior/Warrior.Build.cs
@@ -8,7 +8,7 @@ public class Warrior : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks", "AnimGraphRuntime", "MotionWarping", "Niagara", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "EnhancedInput", "GameplayTasks", "AnimGraphRuntime", "MotionWarping", "Niagara", "NavigationSystem", "MoviePlayer" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
